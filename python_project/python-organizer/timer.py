13a183e4a49f6d03982e2bff474fd28c61f02599
Cha Capt <kimwansl21@gmail.com>
2026-07-26 04:46:15 +0900

    타이머 2차

--- python_project/python-organizer/timer.py
+++ python_project/python-organizer/timer.py
@@ -1,13 +1,17 @@
-
 import time
+focus_minutes = int(input("집중할 시간을 입력하세요:"))
 
-remaining_time = int(input("시간을 입력하세요:"))
+focus_Seconds = focus_minutes * 60
 
-while remaining_time > 0:
-    print(round(remaining_time,1))
+while focus_Seconds > 0:
+    minutes = focus_Seconds // 60
+    
+    seconds = focus_Seconds % 60
 
-    time.sleep(0.1)
-
-    remaining_time -= 0.1
+    print(f"{minutes:02d}:{seconds:02d}")
+    
+    time.sleep(1)
+   
+    focus_Seconds-=1
 
 print("타이머가 종료되었습니다")
